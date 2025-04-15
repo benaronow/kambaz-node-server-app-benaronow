0f4bef02c3781059d074719fc3707ea040352ebe
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-04-14 22:21:46 -0400

    Assignment 6
    assignments
    
    some changes
    
    Revert "Assignment 6"
    
    guided
    
    assignments
    
    some changes

--- src/Kambaz/Enrollments/dao.js
+++ src/Kambaz/Enrollments/dao.js
@@ -1,20 +1,19 @@
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
-export function findEnrollments() {
-  return Database.enrollments;
+export async function findCoursesForUser(userId) {
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
 }
-export function enrollUserInCourse(enrollment) {
-  const { enrollments } = Database;
-  const newEnrollment = {
-    _id: uuidv4(),
-    user: enrollment.user,
-    course: enrollment.course,
-  };
-  enrollments.push(newEnrollment);
-  return newEnrollment;
+
+export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
 }
-export function unenrollUserInCourse(enrollmentId) {
-  const { enrollments } = Database;
-  Database.enrollments = enrollments.filter((e) => e._id !== enrollmentId);
+
+export function enrollUserInCourse(user, course) {
+  return model.create({ user, course, _id: `${user}-${course}` });
+}
+
+export function unenrollUserFromCourse(user, course) {
+  return model.deleteOne({ user, course });
 }
