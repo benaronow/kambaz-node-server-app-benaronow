e715e38210ba25c7f9a89b39b05becc1b96b2e67
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-04-15 00:50:55 -0400

    assignments

--- src/Kambaz/Assignments/routes.js
+++ src/Kambaz/Assignments/routes.js
@@ -1,27 +1,35 @@
 import * as dao from "./dao.js";
 
 export default function AssignmentRoutes(app) {
-  app.delete("/api/assignments/:assignmentId", (req, res) => {
-    const { assignmentId } = req.params;
-    const status = dao.deleteAssignment(assignmentId);
-    res.send(status);
-  });
-  app.put("/api/assignments/:assignmentId", (req, res) => {
-    const { assignmentId } = req.params;
-    const assignmentUpdates = req.body;
-    const status = dao.updateAssignment(assignmentId, assignmentUpdates);
-    res.send(status);
-  });
-  app.get("/api/assignments/:courseId", (req, res) => {
+  const findDbAssignments = async (req, res) => {
     const { courseId } = req.params;
-    const assignments = dao.findAssignmentsForCourse(courseId);
+    const assignments = await dao.findAssignmentsForCourse(courseId);
     res.json(assignments);
-  });
-  app.post("/api/assignments", (req, res) => {
+  };
+
+  const createDbAssignment = async (req, res) => {
     const assignment = {
       ...req.body,
     };
-    const newAssignment = dao.createAssignment(assignment);
+    const newAssignment = await dao.createAssignment(assignment);
     res.send(newAssignment);
-  });
+  };
+
+  const updateDbAssignment = async (req, res) => {
+    const { assignmentId } = req.params;
+    const assignmentUpdates = req.body;
+    const status = await dao.updateAssignment(assignmentId, assignmentUpdates);
+    res.send(status);
+  };
+
+  const deleteDbAssignment = async (req, res) => {
+    const { assignmentId } = req.params;
+    const status = await dao.deleteAssignment(assignmentId);
+    res.send(status);
+  };
+
+  app.get("/api/assignments/:courseId", findDbAssignments);
+  app.post("/api/assignments", createDbAssignment);
+  app.put("/api/assignments/:assignmentId", updateDbAssignment);
+  app.delete("/api/assignments/:assignmentId", deleteDbAssignment);
 }
